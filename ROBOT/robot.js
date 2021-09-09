7d66efcf0791077f29622024d622b692b77264a6
abdugafurovayubxon <87429344+abdugafurovayubxon@users.noreply.github.com>
2021-09-09 10:12:37 +0500

    Add files via upload

---
+++ ROBOT/robot.js
@@ -0,0 +1,13 @@
+var robot=document.getElementById('robot');
+function stop(){
+    robot.animationName='none';
+}
+function run(){
+    robot.animationName='Running';
+}
+function wave(){
+    robot.animationName='Wave';
+}
+function jump(){
+    robot.animationName='Jump';
+}
